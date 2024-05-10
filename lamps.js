e167b22dcca58821ef60c3d93800f4df2a8ec587
shananiki <imnotniki@gmx.de>
2024-05-10 12:19:01 +0200

    Something is not working yet.

--- lamps.js
+++ lamps.js
@@ -1,5 +1,6 @@
 require ('dotenv').config();
 const express = require('express');
+const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const mysql = require('mysql2/promise');
@@ -93,10 +94,59 @@ app.get('/images/:id', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+// Configure Multer for file uploads
+const upload = multer({
+	dest: 'images/',
+	filename: (req, file, cb) => {
+	  cb(null, getNextLampID() + '.' + getExtension(file.originalname));
+	}
+  });
 
+// Get next lampID
+async function getNextLampID() {
+	const [rows] = await pool.query('SELECT AUTO_INCREMENT AS nextID FROM information_schema.TABLES WHERE TABLE_NAME = "lamps"');
+	return rows[0].nextID;
+}
+
+function getExtension(filename) {
+	return filename.split('.').pop();
+}
+
+// Upload route
+app.post('/upload', upload.single('image'), async (req, res) => {
+	const { description, date } = req.body;
+	const image = req.file.filename;
+	//const nextID = await getNextLampID();
+  
+	// Check if all fields are provided
+	if (!image || !description || !date) {
+	  return res.status(400).send('Please fill out all fields');
+	}
+  
+	// Insert data into database
+	try {
+
+		const connection = await pool.getConnection();
+		const sql = `INSERT INTO lamps (lampID, image, description, date) VALUES (, ?, ?, ?)`;
+		const [rows] = await connection.execute(sql, [nextID, image, description, date]); 
+		connection.release();
+	  
+	  	res.send('Image uploaded successfully!');
+	} catch (err) {
+	  console.error(err);
+	  res.status(500).send('Internal server error');
+	}
+  });
+
+// Serve upload form
+app.get('/upload', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'upload.html'));
+  });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
